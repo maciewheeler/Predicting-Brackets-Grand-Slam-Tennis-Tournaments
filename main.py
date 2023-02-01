6ad097b1ec4ab385f34349cef3719913bcea5181
Chintan Sawla <59383889+sawlachintan@users.noreply.github.com>
2023-01-31 20:42:09 -0500

    scraper for ATP data

---
+++ main.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+links = [
+    "1st-serve",
+    "1st-serve-points-won",
+    "2nd-serve-points-won",
+    "service-games-won",
+    "1st-serve-return-points-won",
+    "break-points-saved",
+    "2nd-serve-return-points-won",
+    "break-points-converted",
+    "return-games-won",
+    "aces",
+]
+
+
+for link in links:
+    page = requests.get(f'https://www.atptour.com/en/stats/{link}/2019/hard/all/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    tables = soup.select('#statsListingTableContent > table')[0]
+
+
+    for table in tables:
+        
+        head = table.text.strip().split('\n')
+        head = [x for x in head if x != '']
+        if 'Player' in head:
+            df = pd.DataFrame(columns=head)
+            row_len = len(head) + 1
+            cols = head
+            print(head)
+        
+        if len(head) != 0 and 'Player' not in head:
+
+            for idx in range(0, len(head), row_len):
+                row = head[idx+1:idx+row_len]
+                df.loc[len(df)] = row
+            
+            df.to_csv(f'./data/{link}.csv', index=False)
+
+        
+
