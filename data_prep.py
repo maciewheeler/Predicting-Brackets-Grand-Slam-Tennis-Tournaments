80d41fe39f19442ecbe156927b7606bfc0f87e4d
maciewheeler <wheele73@purdue.edu>
2023-02-20 20:14:17 -0500

    i messed this up, fixing the top 16

--- data_prep.py
+++ data_prep.py
@@ -3,84 +3,81 @@ import pandas as pd
 train = pd.read_csv('data/aus-open-player-stats-2018.csv')
 test = pd.read_csv('data/aus-open-2019.csv')
 
-count = 0
-for i in train["1st_serve"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "1st_serve"] = percent
-    count = count + 1
+# count = 0
+# for i in train["1st_serve"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "1st_serve"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["1st_serve_points_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "1st_serve_points_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["2nd_serve_points_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "2nd_serve_points_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["break_points_saved"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "break_points_saved"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["service_games_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "service_games_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["total_service_points_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "total_service_points_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["1st_serve_return_points_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "1st_serve_return_points_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["2nd_serve_return_points_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "2nd_serve_return_points_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["break_points_converted"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "break_points_converted"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["return_games_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "return_games_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["return_points_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "return_points_won"] = percent
+#     count = count + 1
+#
+# count = 0
+# for i in train["total_points_won"]:
+#     percent = float(i.strip("%")) / 100
+#     train.loc[count, "total_points_won"] = percent
+#     count = count + 1
+#
+# train.to_csv('data/aus-open-player-stats-2018.csv')
 
-count = 0
-for i in train["1st_serve_points_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "1st_serve_points_won"] = percent
-    count = count + 1
+new16 = test[test["Round"] == "4th Round"]
+new_test16 = new16[["Winner", "Loser", "WRank", "LRank"]]
 
-count = 0
-for i in train["2nd_serve_points_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "2nd_serve_points_won"] = percent
-    count = count + 1
-
-count = 0
-for i in train["break_points_saved"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "break_points_saved"] = percent
-    count = count + 1
-
-count = 0
-for i in train["service_games_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "service_games_won"] = percent
-    count = count + 1
-
-count = 0
-for i in train["total_service_points_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "total_service_points_won"] = percent
-    count = count + 1
-
-count = 0
-for i in train["1st_serve_return_points_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "1st_serve_return_points_won"] = percent
-    count = count + 1
-
-count = 0
-for i in train["2nd_serve_return_points_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "2nd_serve_return_points_won"] = percent
-    count = count + 1
-
-count = 0
-for i in train["break_points_converted"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "break_points_converted"] = percent
-    count = count + 1
-
-count = 0
-for i in train["return_games_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "return_games_won"] = percent
-    count = count + 1
-
-count = 0
-for i in train["return_points_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "return_points_won"] = percent
-    count = count + 1
-
-count = 0
-for i in train["total_points_won"]:
-    percent = float(i.strip("%")) / 100
-    train.loc[count, "total_points_won"] = percent
-    count = count + 1
-
-train.to_csv('data/aus-open-player-stats-2018.csv')
-
-new8 = test[test["Round"] == "4th Round"]
-new16 = test[test["Round"] == "3rd Round"]
-new_test8 = new8[["Winner", "WRank"]]
-new_test16 = new16[["Winner", "WRank"]]
-
-new_test8.to_csv("data/aus-open-2019-top-8.csv")
 new_test16.to_csv("data/aus-open-2019-top-16.csv")
